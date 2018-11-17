6cf69e0e8403a048d51a55d0a9c50baf8de301e0
Sathvik Rijo <30945817+sathvikrijo@users.noreply.github.com>
2018-11-18 01:24:01 +0530

    Update handles.py

--- handles.py
+++ handles.py
@@ -2,9 +2,11 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import KeyboardButton, ReplyKeyboardMarkup
 from cricket import *
 keyboardPage0 = [[KeyboardButton("Cricket")],[KeyboardButton("Football")],[KeyboardButton("Tennis")]]
-keyboardPage1 = [[KeyboardButton("1"),KeyboardButton("2"),KeyboardButton("3")],[KeyboardButton("4"),KeyboardButton("5"),KeyboardButton("6")],[KeyboardButton("7"),KeyboardButton("8"),KeyboardButton("9")],[KeyboardButton("10"),KeyboardButton("11"),KeyboardButton("12")]]
+keyboardPage1 = [[KeyboardButton("1"),KeyboardButton("2"),KeyboardButton("3")],[KeyboardButton("4"),KeyboardButton("5"),KeyboardButton("6")],[KeyboardButton("7"),KeyboardButton("8"),KeyboardButton("9")],[KeyboardButton("10"),KeyboardButton("11"),KeyboardButton("Next page")]]
+keyboardPage2 = [[KeyboardButton("12"),KeyboardButton("13"),KeyboardButton("14")],[KeyboardButton("15"),KeyboardButton("16"),KeyboardButton("17")],[KeyboardButton("Prev page"),KeyboardButton("18"),KeyboardButton("19")]]
 sports_reply = ReplyKeyboardMarkup(keyboardPage0,True,False)
-no_reply = ReplyKeyboardMarkup(keyboardPage1,True,False)
+no_reply1 = ReplyKeyboardMarkup(keyboardPage1,True,False)
+no_reply2 = ReplyKeyboardMarkup(keyboardPage2,True,False)
 
 def start(bot,update):
     first_name = update.message.chat.first_name
@@ -14,7 +16,6 @@ def start(bot,update):
         user_name = first_name
     else:
         user_name = first_name + " " + last_name
-
     welcome_msg = "Welcome " + user_name + "\nI'm your bot speaking.\nI can do these kind of things👇\n\n" \
                     + "1  >>  Cricket Scores\n" + "2  >>  Football Scores\n" + "3  >>  Badmiton Scores"
     bot.send_message(chat_id=update.message.chat.id, text=welcome_msg, reply_markup=sports_reply)
@@ -26,4 +27,8 @@ def textMsg(bot,update):
     msg = str(update.effective_message.text)
     if msg == 'Cricket':
         match_titles = match()
-        bot.send_message(chat_id=update.message.chat_id,text=match_titles, reply_markup=no_reply)
+        bot.send_message(chat_id=update.message.chat_id,text=match_titles, reply_markup=no_reply1)
+    elif msg == 'Next page':
+        bot.send_message(chat_id=update.message.chat_id,text=msg, reply_markup=no_reply2)
+    elif msg == 'Prev page':
+        bot.send_message(chat_id=update.message.chat_id,text=msg, reply_markup=no_reply1)
