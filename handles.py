0877ff4c0af002d48b1141888b6ba4fd57935f6f
Sathvik Rijo <30945817+sathvikrijo@users.noreply.github.com>
2018-11-23 09:38:22 +0530

    Updated handles.py

--- handles.py
+++ handles.py
@@ -1,12 +1,16 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import KeyboardButton, ReplyKeyboardMarkup
 from cricket import *
-keyboardPage0 = [[KeyboardButton("Cricket")],[KeyboardButton("Football")],[KeyboardButton("Tennis")]]
+import cricket
+#keyboardPage0 = [[KeyboardButton("Cricket")],[KeyboardButton("Football")],[KeyboardButton("Tennis")]]
 keyboardPage1 = [[KeyboardButton("1"),KeyboardButton("2"),KeyboardButton("3")],[KeyboardButton("4"),KeyboardButton("5"),KeyboardButton("6")],[KeyboardButton("7"),KeyboardButton("8"),KeyboardButton("9")],[KeyboardButton("10"),KeyboardButton("11"),KeyboardButton("Next page")]]
-keyboardPage2 = [[KeyboardButton("12"),KeyboardButton("13"),KeyboardButton("14")],[KeyboardButton("15"),KeyboardButton("16"),KeyboardButton("17")],[KeyboardButton("Prev page"),KeyboardButton("18"),KeyboardButton("19")]]
-sports_reply = ReplyKeyboardMarkup(keyboardPage0,True,False)
+keyboardPage2 = [[KeyboardButton("12"),KeyboardButton("13"),KeyboardButton("14")],[KeyboardButton("15"),KeyboardButton("16"),KeyboardButton("17")],[KeyboardButton("18"),KeyboardButton("19"),KeyboardButton("20")],[KeyboardButton("Prev page"),KeyboardButton("21"),KeyboardButton("Next page")]]
+keyboardPage3 = [[KeyboardButton("22"),KeyboardButton("23"),KeyboardButton("24")],[KeyboardButton("25"),KeyboardButton("26"),KeyboardButton("27")],[KeyboardButton("28"),KeyboardButton("29"),KeyboardButton("30")],[KeyboardButton("Prev page"),KeyboardButton("31"),KeyboardButton("32")]]
+#sports_reply = ReplyKeyboardMarkup(keyboardPage0,True,False)
 no_reply1 = ReplyKeyboardMarkup(keyboardPage1,True,False)
 no_reply2 = ReplyKeyboardMarkup(keyboardPage2,True,False)
+no_reply3 = ReplyKeyboardMarkup(keyboardPage3,True,False)
+var = 0
 
 def start(bot,update):
     first_name = update.message.chat.first_name
@@ -16,19 +20,50 @@ def start(bot,update):
         user_name = first_name
     else:
         user_name = first_name + " " + last_name
-    welcome_msg = "Welcome " + user_name + "\nI'm your bot speaking.\nI can do these kind of things👇\n\n" \
-                    + "1  >>  Cricket Scores\n" + "2  >>  Football Scores\n" + "3  >>  Badmiton Scores"
-    bot.send_message(chat_id=update.message.chat.id, text=welcome_msg, reply_markup=sports_reply)
+    #welcome_msg = "Welcome " + user_name + "\nI'm your bot speaking.\nI can do these kind of things👇\n\n" \
+                    #+ "1  >>  Cricket Scores\n" + "2  >>  Football Scores\n" + "3  >>  Badmiton Scores"
+    welcome_msg = "Welcome " + user_name + "\nI'm your bot speaking.\nChoose any of the following matches from above👇"
+    matcheses = match()
+    bot.send_message(chat_id=update.message.chat.id, text=matcheses)
+    bot.send_message(chat_id=update.message.chat.id, text=welcome_msg, reply_markup=no_reply1)
 
 def valid(bot,update):
     bot.send_message(chat_id=update.message.chat_id, text="You not allowed to call these commands, I'm Sorry")
 
 def textMsg(bot,update):
     msg = str(update.effective_message.text)
-    if msg == 'Cricket':
-        match_titles = match()
-        bot.send_message(chat_id=update.message.chat_id,text=match_titles, reply_markup=no_reply1)
-    elif msg == 'Next page':
-        bot.send_message(chat_id=update.message.chat_id,text=msg, reply_markup=no_reply2)
+    global var
+    if msg == 'Next page':
+        if var == 0:
+            bot.send_message(chat_id=update.message.chat_id, text=msg, reply_markup=no_reply2)
+            var+=1
+        elif var == 1:
+            bot.send_message(chat_id=update.message.chat_id, text=msg, reply_markup=no_reply3)
+            var+=1
+        print(var)
     elif msg == 'Prev page':
-        bot.send_message(chat_id=update.message.chat_id,text=msg, reply_markup=no_reply1)
+        if var == 1:
+            bot.send_message(chat_id=update.message.chat_id, text=msg, reply_markup=no_reply1)
+            var-=1
+        elif var == 2:
+            bot.send_message(chat_id=update.message.chat_id, text=msg, reply_markup=no_reply2)
+            var-=1
+        print(var)
+    else:
+        value = validate(msg)
+        link_no = int(msg)
+        if value == True:
+            match_name = extract_live_match(cricket.live_match_links[link_no-1])
+        else:
+            #print(link_no-cricket.live_match_len-1)
+            match_name = extract_completed_match(cricket.completed_match_links[link_no-cricket.live_match_len-1])
+        bot.send_message(chat_id=update.message.chat_id, text=match_name)
+
+def validate(m):
+    m = int(m)
+    if m <= cricket.live_match_len:
+        print("True")
+        return True
+    elif m <= cricket.live_match_len + cricket.completed_match_len:
+        print("False")
+        return False
