3552ce308904c4f88ac6e25d81c560654492ef90
Sathvik Rijo <30945817+sathvikrijo@users.noreply.github.com>
2018-11-23 09:37:39 +0530

    Updated cricket.py

--- cricket.py
+++ cricket.py
@@ -1,27 +1,30 @@
 import requests
-from emoji_handler import *
 from bs4 import BeautifulSoup
 from emoji import emojize
 
 url = "https://www.cricbuzz.com/cricket-match/live-scores"
+live_match = []
+live_match_links = []
+completed_match = []
+completed_match_links = []
+all_matches = []
+ongoing_matches = ""
+finished_matches = ""
+live_match_len = 0
+completed_match_len = 0
 
 def match():
+
     page = requests.get(url)
     soup = BeautifulSoup(page.text,'html.parser')
     headers = soup.find_all(class_="cb-col cb-col-100 cb-lv-main")
 
-    live_match = []
-    live_match_links = []
-    completed_match = []
-    completed_match_links = []
-    all_matches = []
-    count = 1
-    all_matches_string = ""
-    ongoing_matches = ""
-    finished_matches = ""
 
     first_links = soup.find(class_='text-hvr-underline text-bold')
     first_links.decompose()
+
+    global live_match
+    global completed_match
 
     for head in headers:
         match_live = head.find_all(class_="cb-lv-scrs-well cb-lv-scrs-well-live")
@@ -33,20 +36,88 @@ def match():
             completed_match.append(match['title'])
             completed_match_links.append(match['href'])
 
+    global live_match_len
+    global completed_match_len
+    live_match_len = len(live_match)
+    completed_match_len = len(completed_match)
+
+    all_matches_string = ""
+    count = 1
+
+    all_matches_string += "*************LIVE MATCHES*************\n"
     for i in range(0,len(live_match)):
         print(live_match[i])
         print(url + live_match_links[i])
-        emoji = get_emoji(count)
-        all_matches_string += str(emoji) + "  " + live_match[i] + "\n"
+        all_matches_string += str(count) + " >>> " + live_match[i] + "\n\n"
         count+=1
-    print()
+
+    all_matches_string += "*********COMPLETED MATCHES*********\n"
     for i in range(0,len(completed_match)):
         print(completed_match[i])
         print(url + completed_match_links[i])
-        emoji = get_emoji(count)
-        all_matches_string += str(emoji) + "  " + completed_match[i] + "\n"
+        all_matches_string += str(count) + " >>> " + completed_match[i] + "\n\n"
         count+=1
-
     return all_matches_string
+
+def extract_live_match(match_url):
+    match_page = requests.get("https://www.cricbuzz.com/" + match_url)
+    soup = BeautifulSoup(match_page.text,'html.parser')
+
+    match_details = ""
+    main_content = soup.find(class_="cb-col cb-col-67 cb-nws-lft-col cb-comm-pg")
+
+    match_header = main_content.find(class_="cb-min-bat-rw")
+
+    prevSession = main_content.find_all(class_="cb-col cb-col-67 cb-scrs-wrp")
+    prevSession = prevSession[0]
+    info = prevSession.find_all(class_="cb-text-stump")
+    prevSession = prevSession.find(class_="cb-text-gray cb-font-16")
+    if prevSession is not None:
+        prevSession = prevSession.contents[0][1:]
+        match_details += "Previous session: " + prevSession + "\n\n"
+
+    rr = main_content.find(class_="cb-font-12 cb-text-gray")
+    rr = rr.find_all("span")
+    rr = rr[1].text
+
+    match_score = match_header.find(class_="cb-font-20 text-bold")
+    #match_current_status =  match_current_status[0]
+
+    match_details += "Current session: " + match_score.contents[0][1:] + "\n"
+    match_details += "Current RR: " + rr +"\n"
+
+    if info != []:
+        info = info[0].contents[0]
+        match_details += info
+
+    print(match_details)
+    print()
+    return match_details
+
+def extract_completed_match(match_url):
+    match_page = requests.get("https://www.cricbuzz.com/" + match_url)
+    soup = BeautifulSoup(match_page.text,'html.parser')
+
+    match_details = ""
+    main_content = soup.find(class_="cb-col cb-col-67 cb-nws-lft-col cb-comm-pg")
+    result = main_content.find(class_="cb-col cb-col-100 cb-min-stts cb-text-mom")
+    if result is None:
+        result = main_content.find(class_="cb-col cb-col-100 cb-min-stts cb-text-complete")
+    result = result.contents[0]
+
+    match_header = main_content.find_all(class_="cb-col cb-col-100 cb-col-scores")
+    match_header = match_header[0]
+
+    Session = match_header.find(class_="cb-col cb-col-100 cb-min-tm cb-text-gray")
+    match_details += Session.contents[0] + "\n"
+
+    Session = match_header.find(class_="cb-col cb-col-100 cb-min-tm")
+    match_details += Session.contents[0] + "\n"
+
+    match_details += "\n" + result
+    print(match_details)
+    return match_details
+
 if __name__ == "__main__":
     match()
+    #extract_completed_match(completed_match_links[3])
